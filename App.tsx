8e0d39472766260f565dcb787d5a8a1f52ed8d24
Damian <dg997678@gmail.com>
2025-01-27 23:29:54 -0500

    Add persisten in the store

--- App.tsx
+++ App.tsx
@@ -1,30 +1,23 @@
 import 'react-native-reanimated';
-import React, {useEffect} from 'react';
+import React from 'react';
 import MainRoutes from './src/navigation/mainNavigation';
 import Toast from 'react-native-toast-message';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {Provider} from 'react-redux';
-import store from './src/redux/store';
-// import database from './src/utils/database/SQLiteDatabase';
-
-// const resetDatabase = () => {
-//   database.transaction(tx => {
-//     tx.executeSql('DROP TABLE IF EXISTS habits');
-//     tx.executeSql('DROP TABLE IF EXISTS another_table');
-//   });
-// };
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistor, store} from './src/redux/store';
 
 const App = () => {
-  useEffect(() => {
-    // resetDatabase();
-  }, []);
-
   return (
     <Provider store={store}>
-      <GestureHandlerRootView style={{flex: 1}}>
-        <MainRoutes />
-        <Toast />
-      </GestureHandlerRootView>
+      <PersistGate
+        loading={<GestureHandlerRootView style={{flex: 1}} />}
+        persistor={persistor}>
+        <GestureHandlerRootView style={{flex: 1}}>
+          <MainRoutes />
+          <Toast />
+        </GestureHandlerRootView>
+      </PersistGate>
     </Provider>
   );
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,33 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {configureStore, combineReducers} from '@reduxjs/toolkit';
+import {persistStore, persistReducer} from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import tokenReducer from './tokenSlice';
 import challengeReducer from './challengeSlice';
 
-const store = configureStore({
-  reducer: {
-    token: tokenReducer,
-    challenge: challengeReducer,
-  },
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['token'],
+};
+
+const rootReducer = combineReducers({
+  token: tokenReducer,
+  challenge: challengeReducer,
 });
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
